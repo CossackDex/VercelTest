df0826eeeaaa0c70f51d340c9223c17146cd2e77
CossackDex <d.matuszczyk98@gmail.com>
2023-01-25 02:21:27 +0100

    vercel deploy

--- server.py
+++ server.py
@@ -74,6 +74,4 @@ def logout():
         )
     )
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=env.get("PORT", 3000), debug=True)
+app.run(host="0.0.0.0", port=env.get("PORT", 3000), debug=True)
